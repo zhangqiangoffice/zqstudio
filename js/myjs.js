37684ee67a358d116b72f1955c3e45433c29c1b7
Zhang Qiang <zhangqiangoffice@163.com>
2015-05-27 10:47:26 +0800

    how to upload?
    how to upload?

---
+++ js/myjs.js
@@ -0,0 +1,15 @@
+﻿window.onload=function(){
+  var oMenus=document.getElementById('menus');
+  var aSpan=oMenus.getElementsByTagName('span');    //菜单树打开收起功能
+  var test=aSpan.length;
+  for(var i=0;i<aSpan.length;i++){
+     aSpan[i].onclick=function(){
+	    var aUl=this.parentNode.getElementsByTagName('ul');
+		var oStyle=aUl[0].style.display;
+		if (oStyle!="none"){
+	    aUl[0].style.display="none";
+		} else {
+		aUl[0].style.display="block";}
+  }
+  }
+}
